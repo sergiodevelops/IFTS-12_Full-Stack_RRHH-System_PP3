22981a782a48ed7d875ebcc77aa86b05305744f9
Cheko <sergio.juarez@zoomagri.com>
2021-08-02 17:21:32 -0300

    add clean architecture in rrhh-react project

---
+++ src/constants/types.js
@@ -0,0 +1,8 @@
+// import IClassificationModelsRepository from "@application/repositories/IClassificationModelsRepository";
+// import IWeightModelsRepository from "@application/repositories/IWeightModelsRepository";
+// import SynchronizationService from "@configuration/usecases/SynchronizationService";
+
+export const TYPES = {
+    // IAnalysisRepository: Symbol.for("IAnalysisRepository"),
+    // AnalysisService: Symbol.for("AnalysisService"),
+}
