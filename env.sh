811c61f352692ea9350d24960e922ed2235bed11
Cheko <sergio.juarez@zoomagri.com>
2021-08-03 00:36:40 -0300

    borrando

--- env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# https://github.com/kunokdev/cra-runtime-environment-variables
-
-# line endings must be \n, not \r\n !
-echo "window._env = {" > ./env.js
-awk -F '=' '{ print $1 ": \"" (ENVIRON[$1] ? ENVIRON[$1] : $2) "\"," }' ./.env >> ./env.js
-echo "}" >> ./env.js
