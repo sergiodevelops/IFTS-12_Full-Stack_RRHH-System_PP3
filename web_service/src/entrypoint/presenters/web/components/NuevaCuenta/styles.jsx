ba9257e9e3bbe6736d5e82891f95f7e6ba76162c
Cheko <sergio.juarez@zoomagri.com>
2021-09-19 19:12:36 -0300

    correcciones de estilo visuales titulo alineación al centro

--- web_service/src/entrypoint/presenters/web/components/NuevaCuenta/styles.jsx
+++ web_service/src/entrypoint/presenters/web/components/NuevaCuenta/styles.jsx
@@ -1,6 +1,9 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(theme => ({
+    titulo:{
+        textAlign: "center",
+    },
     formControl: {
         minWidth: '100%',
         '& .MuiAutocomplete-root':{
