bf452869f35ad06ba989eb16b03402bf25495d71
Cheko <sergio.juarez@zoomagri.com>
2021-09-25 16:07:42 -0300

    acomodamos la logica visual para que haya un login o un registrar o si ya esta registrado ingrese al home del sistema

--- web_service/src/entrypoint/presenters/web/components/Tabs/Tabs.jsx
+++ web_service/src/entrypoint/presenters/web/components/Tabs/Tabs.jsx
@@ -21,19 +21,20 @@ function Tabs(){
             <TabContext value={value}>
                 <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                     <TabList onChange={handleChange} aria-label="lab API tabs example">
-                        <Tab label="Sign In" value="1" />
-                        <Tab label="Sign Up" value="2" />
+                        <Tab label="Administrativo" value="1" />
+                        <Tab label="Solicitante" value="2" />
+                        <Tab label="Postulante" value="3" />
                     </TabList>
                 </Box>
 
                 <TabPanel value="1">
-                    <Autenticacion/>
+                    Sección para el Usuario "Administrativo"...
                 </TabPanel>
                 <TabPanel value="2">
-                    <Principal/>
+                    Sección para el Usuario "Solicitante"...
                 </TabPanel>
                 <TabPanel value="3">
-                    <NuevaCuenta/>
+                    Sección para el Usuario "Postulante"...
                 </TabPanel>
 
             </TabContext>
--- web_service/src/entrypoint/presenters/web/pages/Principal/Principal.jsx
+++ web_service/src/entrypoint/presenters/web/pages/Principal/Principal.jsx
@@ -11,12 +11,30 @@ import useStyles from "./styles";
 import Autenticacion from "../../components/Autenticacion/Autenticacion";
 import NuevaCuenta from "../../components/NuevaCuenta/NuevaCuenta";
 import Tabs from "../../components/Tabs/Tabs";
+import * as PropTypes from "prop-types";
 
 // https://www.williamkurniawan.com/blog/building-a-simple-login-form-with-material-ui-and-react-hook-form
 
+function SignInUpSwitch(props) {
+    return <Grid container spacing={3}>
+        <Grid item xs={12}>
+            <Button color="secondary" fullWidth type="submit" variant="contained" onClick={props.onClick}>
+                {
+                    props.existeUsuario ? "crear nueva cuenta" : "volver"
+                }
+            </Button>
+        </Grid>
+    </Grid>;
+}
+
+SignInUpSwitch.propTypes = {
+    onClick: PropTypes.func,
+    existeUsuario: PropTypes.bool
+};
+
 function Principal() {
     const [existeUsuario, setExisteUsuario] = useState(true);
-    const [inicioSesion, setInicioSesion] = useState(true);
+    const [inicioSesion, setInicioSesion] = useState(false);
     const classes = useStyles();
     const handleClick = (event) => {
         setExisteUsuario(!existeUsuario)
@@ -25,24 +43,12 @@ function Principal() {
     return (
         <Container className={classes.container} maxWidth="xs">
             {
-                inicioSesion
-                    ?
-                    <Tabs/>
-                    :
-                    (
-                        existeUsuario ? <Autenticacion/> : <NuevaCuenta/>
-                    )
+                inicioSesion ?
+                    (<Tabs/>) :
+                    (existeUsuario ?<Autenticacion/> : <NuevaCuenta/>)
             }
             {
-                <Grid container spacing={3}>
-                    <Grid item xs={12}>
-                        <Button color="secondary" fullWidth type="submit" variant="contained" onClick={handleClick}>
-                            {
-                                existeUsuario ? "crear nueva cuenta" : "volver"
-                            }
-                        </Button>
-                    </Grid>
-                </Grid>
+                !inicioSesion && <SignInUpSwitch onClick={handleClick} existeUsuario={existeUsuario}/>
             }
         </Container>
     );
